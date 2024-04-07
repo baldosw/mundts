d8c65c4053d4b5e1eaf5dd0cd0fdd6638bdebacd
ThinkCentre_i7 <baldoswill@gmail.com>
2024-04-07 20:59:10 +0800

    Add change contact number name

---
+++ DTS.DataAccess/Migrations/20240407112856_ChangeContactNumberOneToContactNumber.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DTS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeContactNumberOneToContactNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContactNumberOne",
+                table: "Employees",
+                newName: "ContactNumber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ContactNumber",
+                table: "Employees",
+                newName: "ContactNumberOne");
+        }
+    }
+}
