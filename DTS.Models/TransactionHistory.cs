711ec003808784052cf7819c034ad56b43f68976
ThinkCentre_i7 <baldoswill@gmail.com>
2024-04-16 07:31:17 +0800

    add TransactionHistory

---
+++ DTS.Models/TransactionHistory.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace DTS.Models;
+
+public class TransactionHistory
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int DocumentId { get; set; }
+
+    [ValidateNever]
+    public Document Document { get; set; }
+
+    [Required]
+    public int StatusId { get; set; }
+
+    [ValidateNever]
+    public Status Status { get; set; }
+
+    public string Remarks { get; set; }
+
+    public int ModifiedBy { get; set; }
+
+    public DateTime ModifiedDate { get; set; }
+}
